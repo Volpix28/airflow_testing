e56540f7f9a4a372bfee60b92273c7425e0ca24d
Volpix28 <alexander.roucka@gmail.com>
2024-03-06 11:46:36 +0100

    add hello world DAG

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,1 +1,29 @@
-import airflow
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'hello_airflow',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+def print_hello():
+    return 'Hello Airflow!'
+
+t1 = PythonOperator(
+    task_id='print_hello',
+    python_callable=print_hello,
+    dag=dag,
+)
